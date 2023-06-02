016e8098202ddb0c7ecf8454b303a76692600ba1
Antoine Mauboussin <antoine.mauboussin@livecampus.tech>
2023-06-02 17:01:25 +0200

    basic register

--- src/project/app/urls.py
+++ src/project/app/urls.py
@@ -2,13 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 
-from .views import index,compute_square,compute_squares,random_wiki,form_prospect
+from .views import index,register
 
 urlpatterns = [
     path("", index, name="index"),
-    path("compute_square/<int:number>", compute_square, name="compute_square"),
-    path("compute_squares/<int:number>", compute_squares, name="compute_squares"),
-    path("random_wiki", random_wiki, name="random_wiki"),
-    path("form_prospect", form_prospect, name="form_prospect"),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/register', register, name="register"),
 ]
--- src/project/app/views.py
+++ src/project/app/views.py
@@ -3,59 +3,31 @@ from django.http import HttpResponse
 from django.views.decorators.http import require_http_methods
 from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
+from django import forms
+from app.forms import RegisterForm
+from django.contrib.auth.models import User
 
 def index(request):
     # return HttpResponse(f"Hi")
     return render(request, "index.html")
 
-def compute_square(request, number):
-    square = number * number
-    context = {"square": square, "number": number}
-    return render(request, "compute_square.html", context=context)
+def register(request):
+    validation = False
 
-def compute_squares(request, number):
-    numbers = list(range(number))
-    squares = [n**2 for n in numbers]
-    context = {
-        "number_and_square": [
-            {"number": number, "square": square}
-            for number, square in zip(numbers, squares)
-        ]
-    }
-    return render(request, "compute_squares.html", context=context)
+    if request.method == 'POST':
 
-def random_wiki(request):
-    req = Request('https://en.wikipedia.org/wiki/Special:RandomInCategory/Featured_articles', headers={'User-Agent': 'Mozilla/5.0'})
-    html_page = urlopen(req).read()
-    soup = BeautifulSoup(html_page, 'html.parser')
-    title = soup.title.string
-    lang = []
-    for link in soup.select(".interlanguage-link a"):
-        lang.append(link.get_text())
+        form = RegisterForm(request.POST)
 
-    context = {"lang": lang, "title": title}
-    return render(request, "random_wiki.html", context=context)
+        if form.is_valid():
+            User.objects.create_user(form.cleaned_data['username'], '', form.cleaned_data['password1'])
+            validation = True
     
-from .models import Prospect
+    else:
+        form = RegisterForm()
 
-@require_http_methods(["GET", "POST"])
-def form_prospect(request):
-    if request.method == "POST":
-        first_name = request.POST.get("firstname")
-        last_name = request.POST.get("lastname")
-        tel = request.POST.get("tel")
-        email = request.POST.get("email")
-        message = request.POST.get("message")
+    context = {
+        'form': form,
+        'validation': validation
+    }
 
-        prospect_object = Prospect.objects.create(
-            first_name=first_name,
-            last_name=last_name,
-            tel=tel,
-            email=email,
-            message=message,
-        )
-        prospect_object.save()
-
-        return render(request, "form_received.html")
-    return render(request, "form_prospect.html", context={})
-
+    return render(request, 'registration/register.html', context)
