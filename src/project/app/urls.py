e584b94ca20dfb2f73dfe06409def4d292e6afe5
Antoine Mauboussin <antoine.mauboussin@livecampus.tech>
2023-06-06 15:35:37 +0200

    fix

--- src/project/app/urls.py
+++ src/project/app/urls.py
@@ -10,7 +10,7 @@ from .views import (
     update_item,
     delete_item,
 )
-from .views import index, register, share_item, shared_items, delete_item
+from .views import index, register, share_item, shared_items, delete_item,delete_shared
 
 urlpatterns = [
     path("", index, name="index"),
@@ -20,7 +20,7 @@ urlpatterns = [
     path("items_list", items_list, name="items_list"),
     path("share_item/<int:id>", share_item, name="share_item"),
     path("shared_items", shared_items, name="shared_items"),
-    path("delete_shared/<int:id>", delete_item, name="delete_item"),
+    path("delete_shared/<int:id>", delete_shared, name="delete_shared"),
     path("update_item/<item_id>", update_item, name="update_item"),
     path("delete_item/<item_id>", delete_item, name="delete_item"),
 ]
