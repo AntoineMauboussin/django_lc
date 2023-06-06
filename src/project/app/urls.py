8b8cc00c85d83b61c4e43b9f5ad6577f37045497
Fret <clemfret7@gmail.com>
2023-06-06 11:19:22 +0200

    Update items

--- src/project/app/urls.py
+++ src/project/app/urls.py
@@ -6,6 +6,7 @@ from .views import (
     index,
     create_item,
     items_list,
+    update_item,
     delete_item,
 )
 from .views import index, register
@@ -16,5 +17,6 @@ urlpatterns = [
     path("accounts/register", register, name="register"),
     path("create_item", create_item, name="create_item"),
     path("items_list", items_list, name="items_list"),
-    path("delete_item/<item_id>", delete_item, name="delete_item")
+    path("update_item/<item_id>", update_item, name="update_item"),
+    path("delete_item/<item_id>", delete_item, name="delete_item"),
 ]
--- src/project/app/views.py
+++ src/project/app/views.py
@@ -50,9 +50,34 @@ def create_item(request):
 
     return render(request, "form_item_prospect.html", context={})
 
+
+@login_required
+@require_http_methods(["GET", "POST"])
+def update_item(request, item_id):
+    Item.objects.get
+    item = Item.objects.get(id=item_id)
+
+    if request.method == "POST":
+        item.user_name = request.POST.get("username")
+        item.password = request.POST.get("password")
+        item.url = request.POST.get("url")
+
+        item.save()
+
+        return redirect("items_list")
+
+    reverse_url = reverse("update_item", args=[item.id])
+
+    return render(
+        request,
+        "form_item_update.html",
+        context={"item": item, "reverse_url": reverse_url},
+    )
+
+
 @login_required
 def delete_item(request, item_id):
-    item = Item.objects.filter(id = item_id)
+    item = Item.objects.filter(id=item_id)
     item.delete()
     return redirect(reverse("items_list"))
 
